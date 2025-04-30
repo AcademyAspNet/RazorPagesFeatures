a8edce4b8df09cdcdbff870ddf94cf8f05005461
Academy <Academy@AcademyTop>
2025-04-30 16:27:49 +0300

    Lesson 10: Initial commit

---
+++ Pages/Syntax.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesFeatures.Pages
+{
+    public class SyntaxModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
